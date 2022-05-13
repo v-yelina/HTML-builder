56480042fba1752ce5bae8afcfc4b00b80683f62
v-yelina <heytridvaodin@gmail.com>
2022-05-13 22:44:59 +0200

    feat: finish files in folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+fs.readdir(
+  `${__dirname}/secret-folder`,
+  { withFileTypes: true },
+  (err, files) => {
+    console.log("\nCurrent directory files:");
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          const filePath = `${__dirname}/secret-folder/${file.name}`;
+          const fileName = `${file.name.split(".")[0]}`;
+          const fileExt = `${path.extname(file.name).slice(1)}`;
+          fs.stat(filePath, (err, stats) => {
+            console.log(`${fileName} - ${fileExt} - ${stats.size}B`);
+          });
+        }
+      });
+    }
+  }
+);
