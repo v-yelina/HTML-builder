00c0fbd635e9a1108a564ceadb82cbb85efe01f7
v-yelina <heytridvaodin@gmail.com>
2022-05-13 15:32:51 +0200

    feat: finish write file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const readline = require("readline");
+
+const writable = fs.createWriteStream(`${__dirname}/text.txt`);
+
+console.log("Write your text here:");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.prompt();
+
+rl.on("line", (line) => {
+  switch (line.trim()) {
+    case "exit":
+      rl.close();
+      break;
+    default:
+      sentence = line + "\n";
+      writable.write(sentence);
+      rl.prompt();
+      break;
+  }
+}).on("close", () => {
+  writable.end();
+  writable.on("finish", () => {
+    console.log(`Thank you, you can find your input in ${__dirname}/text.txt`);
+  });
+  setTimeout(() => {
+    process.exit(0);
+  }, 100);
+});
