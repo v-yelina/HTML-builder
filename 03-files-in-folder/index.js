a9c4321bb3f80ba241127d84911640d0c5a08187
v-yelina <heytridvaodin@gmail.com>
2022-05-22 10:30:37 +0200

    fix: paths in files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,25 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readdir(
-  `${__dirname}/secret-folder`,
-  { withFileTypes: true },
-  (err, files) => {
-    console.log("\nCurrent directory files:");
-    if (err) console.log(err);
-    else {
-      files.forEach((file) => {
-        if (file.isFile()) {
-          const filePath = `${__dirname}/secret-folder/${file.name}`;
-          const fileName = `${file.name.split(".")[0]}`;
-          const fileExt = `${path.extname(file.name).slice(1)}`;
-          fs.stat(filePath, (err, stats) => {
-            console.log(
-              `${fileName} - ${fileExt} - ${(stats.size / 1024).toFixed(2)}kB`
-            );
-          });
-        }
-      });
-    }
+const targetDir = path.join(__dirname, "secret-folder");
+
+fs.readdir(targetDir, { withFileTypes: true }, (err, files) => {
+  console.log(`\nFiles in ${targetDir}:`);
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(targetDir, file.name);
+        const fileName = `${file.name.split(".")[0]}`;
+        const fileExt = `${path.extname(file.name).slice(1)}`;
+        fs.stat(filePath, (err, stats) => {
+          console.log(
+            `${fileName} - ${fileExt} - ${(stats.size / 1024).toFixed(2)}kB`
+          );
+        });
+      }
+    });
   }
-);
+});
